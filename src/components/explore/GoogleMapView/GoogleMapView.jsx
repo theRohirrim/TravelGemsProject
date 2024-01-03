b984b1d120c5ab4cdab1a8df06c1fef658a8f73a
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2024-01-03 16:27:51 +0000

    update map load

--- src/components/explore/GoogleMapView/GoogleMapView.jsx
+++ src/components/explore/GoogleMapView/GoogleMapView.jsx
@@ -1,43 +1,81 @@
 "use client"
-import { GoogleMap, LoadScript, MarkerF, useLoadScript } from '@react-google-maps/api';
-import React from 'react';
-import styles from './googleMap.module.css'
+import { GoogleMap, Marker, InfoWindow, useLoadScript } from '@react-google-maps/api';
+import React, { useState } from 'react';
+import Image from 'next/image'
+import style from "./googleMap.module.css"
+import Link from 'next/link';
 
 const GoogleMapView = ({ locations }) => {
-    // size of map 
     const containerStyle = {
         width: '80vw',
         height: '35vh',
-        margin: 'auto' 
-    }
+        margin: 'auto',
+    };
 
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
-      });
-      
+    });
 
-    // starting coords
-    const coords = { lat: 51.51360936836878, lng: -0.08095507300680699 }
+    function stringLimit(str, limit) {
+        if (str.length <= limit) {
+            return str
+        } else {
+            return str.slice(0, limit)
+        }
+    }
+    const coords = { lat: 51.51360936836878, lng: -0.08095507300680699 };
 
-    // display map, centered on coords
+    const [selectLocation, setLocation] = useState("");
+
+    const handleMarkerClick = (location) => {
+        setLocation(location)
+    };
+
+    const handleInfoWindowClose = () => {
+        setLocation("");
+    };
+
     return (
-        <div className={styles.mapViewerWrapper}>
-           
-                {isLoaded && <GoogleMap
-                    mapContainerStyle={containerStyle}
-                    center={coords}
-                    zoom={13}>
+        <div>
+            {isLoaded && <GoogleMap mapContainerStyle={containerStyle} center={coords} zoom={13}>
+                {locations.map((location) => (
+                    <Marker
+                        position={{ lat: location.latitude, lng: location.longitude }}
+                        key={location._id}
+                        onClick={() => handleMarkerClick(location)}
+                    />
+                ))}
 
-                    {locations.map(location => (
-                        <MarkerF
-                            position={{lat: location.latitude, lng: location.longitude}}
-                            key={location._id}
-                        />
-                    ))}
-                </GoogleMap>}
-    
+                {selectLocation && (
+                    <InfoWindow
+                        position={{
+                            lat: selectLocation.latitude,
+                            lng: selectLocation.longitude,
+                        }}
+                        onCloseClick={handleInfoWindowClose}
+                    >
+                        <div className={style.popupWrapper}>
+                            <Image
+                                src={selectLocation.img}
+                                alt=''
+                                width={100}
+                                height={100}
+                            />
+                            <div className={style.popText}>
+
+                                <Link href={`/${selectLocation._id}`}>
+                                    <h4>{selectLocation.place_name}</h4>
+                                </Link>
+                                <p>{selectLocation.category}</p>
+                                <p> {`${stringLimit(selectLocation.description, 100)}...`}</p>
+                            </div>
+                        </div>
+                    </InfoWindow>
+                )}
+            </GoogleMap>
+            }
         </div>
     );
-}
+};
 
 export default GoogleMapView;
