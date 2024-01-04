525abea02db442d654756aca0f6ad42f30dddac2
DominiqueCoding <aanon12589@gmail.com>
2024-01-04 11:07:33 +0000

    added logos

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,9 @@ import styles from './header.module.css'
 const Header = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.logo}>LOGO</div>
+            <div className={styles.logo}>
+                <img src="../Graphic Design/3.png" alt="travel gems logo" />
+            </div>
             <div className={styles.auth}>AUTHENTICATION</div>
         </div>
     )
