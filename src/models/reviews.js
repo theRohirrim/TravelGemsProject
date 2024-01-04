521d161d3bccb444dbcbe815b92c6fc16eed5f1e
hala_jash <starshiny567@gmail.com>
2024-01-04 11:07:47 +0000

    added schema for reviews

---
+++ src/models/reviews.js
@@ -0,0 +1,40 @@
+// reviews /rating /created at /  / ref to location id / references ot user id
+
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  body: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  location_id: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Locations',
+    required: [true, 'Reviews must belong to a location'],
+  },
+  user_id: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Users',
+    required: [true, 'Reviews must belong to a user'],
+  },
+  username: {
+    type: String,
+  },
+  votes: {
+    type: Number,
+  },
+  place_name: {
+    type: String,
+  },
+});
+
+export const Review = mongoose.model('Review', reviewSchema);
