dc57a5e5ea4248ece73a2c6725ce37eda6f3a13d
Josh Holt <jholt@Stephanie-2.local>
2024-01-04 16:23:27 +0000

    completed UI for filters, need to implement logic

--- src/components/explore/ExplorePage.jsx
+++ src/components/explore/ExplorePage.jsx
@@ -1,18 +1,30 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import GoogleMapView from './GoogleMapView/GoogleMapView';
 import styles from './explorepage.module.css'
 import MapAndListButtons from './filterAndView/Map&ListButons/Map&ListButtons';
 import MapsNavigation from './filterAndView/mapsNavigation/MapsNavigation';
 import LocationList from './locationList/LocationList';
 
-const ExplorePage = ({ locations }) => {
+const ExplorePage = ({ allLocations }) => {
+    const [locations, setLocations] = useState(allLocations)
     const [mapView, setMapView] = useState(true)
+    const [filterOptions, setFilterOptions] = useState({})
+
+    useEffect(() => {
+        Object.entries(filterOptions)
+        setLocations(locations.filter(
+            
+        ))
+    }, [filterOptions])
+
+    const filteredArray = yourArray.filter(obj => obj.categories.includes("sports"));
+
 
     return (
         <>
-        <MapsNavigation />
+        <MapsNavigation filterOptions={filterOptions} setFilterOptions={setFilterOptions} />
         <MapAndListButtons mapView={mapView} setMapView={setMapView}/>
         <div className={`${mapView === true && styles.disabled}`}>
         <LocationList locations={locations} />
