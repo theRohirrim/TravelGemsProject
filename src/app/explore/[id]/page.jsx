a64ed37e90748c6b13800e9400b66859cfa688de
Spencer <s.clarkegriffiths@gmail.com>
2024-01-04 16:41:06 +0000

    reviews card further nested for easier conditional rendering of post review

--- src/app/explore/[id]/page.jsx
+++ src/app/explore/[id]/page.jsx
@@ -1,5 +1,6 @@
 import LocationCard from '@/components/location/locationCard/LocationCard';
-import ReviewsCard from '@/components/location/reviews/ReviewsCard';
+import ReviewsCard from '@/components/location/reviews/reviewCards.jsx/ReviewsCard';
+
 
 const SingleLocationPage = async ({ params }) => {
     const id = params.id
