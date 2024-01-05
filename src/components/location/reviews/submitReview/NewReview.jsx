bcfd77120f0d32464b83f3ab6ec41e25070bc6c0
Spencer <s.clarkegriffiths@gmail.com>
2024-01-05 16:33:48 +0000

    5 star rating system added

--- src/components/location/reviews/submitReview/NewReview.jsx
+++ src/components/location/reviews/submitReview/NewReview.jsx
@@ -1,7 +1,49 @@
+"use client"
+
+import { useState } from "react"
+import style from "./NewReview.module.css"
+import { FaStar } from "react-icons/fa"
+import { FaGem } from "react-icons/fa";
+
 const NewReview = () => { 
+
+
+    const StarRating = () => { 
+    const [rating, setRating] = useState(null)
+
+
+    console.log(rating)
     return ( 
-        <p> Here is the space for a new review form</p>
+        <div> 
+        {[...Array(5)].map((star, i) => { 
+           const currentRating = i + 1;
+
+           return (
+            <label key={i}> 
+                <input
+                type="radio"
+                name="rating" 
+                className={style.radioButton}
+                value={currentRating}
+                onClick= {() => {
+                if (rating === currentRating){ 
+                        setRating(null)
+                    } else {   
+                    setRating(currentRating)}}}
+            />
+                <FaGem 
+                className={style.star} 
+                size={50} 
+                color= {currentRating <= rating ? "#ffc107" : "#e4e5e9"}
+                />
+                </label>
+           )  
+        })}
+        </div>
     )
+    }
+
+    return StarRating()
 }
 
 export default NewReview
