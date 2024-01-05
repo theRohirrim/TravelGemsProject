e47be735f774070d57dfe42f953edacf2825ec96
DominiqueCoding <aanon12589@gmail.com>
2024-01-05 11:02:06 +0000

    improve logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,9 +3,12 @@ import styles from './header.module.css'
 const Header = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.logo}>
-                <img src="../Graphic Design/3.png" alt="travel gems logo" />
+            <div className={styles.heading}>
+                <img className={styles.logo} src="../Graphic Design/6.png" alt="travel gems logo" />
+                <img className= {styles.header} src="../Graphic Design/7.png" alt="travel gems logo" />
             </div>
+
+
             <div className={styles.auth}>AUTHENTICATION</div>
         </div>
     )
