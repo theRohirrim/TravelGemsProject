6ccfba520e27b1eed9745b8fa98ee78a9ac2bd3d
Spencer <s.clarkegriffiths@gmail.com>
2024-01-05 22:26:21 +0000

    post needs to be moved to not be client side

--- src/components/location/reviews/submitReview/NewReview.jsx
+++ src/components/location/reviews/submitReview/NewReview.jsx
@@ -5,9 +5,20 @@ import style from "./NewReview.module.css"
 import { FaStar } from "react-icons/fa"
 import { FaGem } from "react-icons/fa";
 
-const NewReview = () => { 
+const NewReview = ({id}) => { 
+
+    // user data currently hard coded to spikeman
+    const userId = "659410c69f7ae624673bafdb"
+    const username = "spikeman"
+
+    // location data currently hard coded to covent garden as NewReview needs to be passed id params when rendered
+    const locationId = "659568dbedc28e2e44f28bc1"
+    const place_name = "Covent Garden"
+
+
     const [rating, setRating] = useState(null)
-    
+    // const [reviewBody, setReviewBody] = useState("")
+
     const StarRating = () => { 
     return ( 
         <div> 
@@ -39,11 +50,50 @@ const NewReview = () => {
     )
     }
 
+    const handleReviewInput = (event) => { 
+        let reviewContent = event.target.value
+        setReviewBody(reviewContent)
+    }
+
+//     const reviewData = {
+//     body: "Hala added a review shesffgjkgjilrgj",
+//     rating: 2,
+//     location_id: "659568dbedc28e2e44f28bc1", 
+//     user_id: "659411769f7ae624673bafde",
+//     username: "admin",
+//     votes: 5,
+//     place_name: "Hala Garden",
+// }
+
+
+// const result = await postReview(reviewData);
+// console.log(result);
+
+    const submitNewReview = (event) => { 
+        // if(reviewBody.length > 0) { 
+        //    postReview(reviewData)
+        //     console.log(result)
+        // }
+        event.preventDefault()
+    }
+
     return ( 
         <form className={style.newReview}>
             <StarRating />
-            <input type="text" placeholder="Share your thoughts.."/> 
-            <button>Add Review</button>
+
+            <textarea 
+            className={style.reviewInput}
+            type="text" 
+            placeholder="Share your thoughts.."
+            onChange={handleReviewInput}
+            wrap="hardsoft"
+            > 
+            </textarea>
+            
+            <button
+            onClick = {submitNewReview}
+            >Add Review</button>
+        
         </form>
     ) 
 }
