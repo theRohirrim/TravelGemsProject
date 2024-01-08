25ce33b013952cc977d274978824add2f5502d5a
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2024-01-08 18:08:32 +0000

    comment

--- src/components/add/NewLocationForm.jsx
+++ src/components/add/NewLocationForm.jsx
@@ -7,32 +7,53 @@ export default function NewLocationForm({latitude, longitude}) {
     const [location, setLocation] = useState("");
     const [category, setCategory] = useState("");
     const [description, setDescription] = useState("");
-    const [img, setImg] = useState([])
+    // const [img, setImg] = useState([])
 
     // address
     // img?
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
         console.log("submitting...")
-        console.log(img, "img?")
-       
-        const newLocation = {
-            created_by: "spikeman",
-            categories: [category],
-            place_name: placeName,
-            address: location,
-            latitude: Number(latitude),
-            longitude: Number(longitude),
-            description,
-            img: "https://images.pexels.com/photos/134061/pexels-photo-134061.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        try {
+            await submitLocation()
+        } catch (error) {
+            console.log(error)
+            throw new Error("handle submit")
         }
-        console.log(newLocation)
-        submitLocation(newLocation)
-        setPlaceName("")
-        setLocation("")
-        setCategory("")
-        setDescription("")
+        
+       
+        // const newLocation = {
+        //     created_by: "spikeman",
+        //     categories: [category],
+        //     place_name: placeName,
+        //     address: location,
+        //     latitude: Number(latitude),
+        //     longitude: Number(longitude),
+        //     description,
+        //     img: "https://images.pexels.com/photos/134061/pexels-photo-134061.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        // }
+        // console.log(newLocation)
+        // try {
+        //     console.log("try...")
+        //     await submitLocation( {
+        //             address: "London",
+        //             categories: ['park'],
+        //             created_by: "spikeman", 
+        //             description: "another park",
+        //             latitude: 51.51123897160571,
+        //             longitude: -0.1708450056152344,
+        //             place_name: "Hyde Park"
+        //         }
+        //     )
+        // } catch (error) {
+        //     console.log(error)
+        //     throw new Error("Could not add location")
+        // }      
+        // setPlaceName("")
+        // setLocation("")
+        // setCategory("")
+        // setDescription("")
     }
 
 
@@ -40,19 +61,19 @@ export default function NewLocationForm({latitude, longitude}) {
         <div>
             <h1>New gem</h1>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="place-name">Place name:</label>
-                <input value={placeName} onChange={e => setPlaceName(e.target.value)}  type="text" id="place-name" placeholder="Leadenhall Market" /><br/>
+                <label htmlFor="placeName">Place name:</label>
+                <input value={placeName} onChange={e => setPlaceName(e.target.value)}  type="text" id="placeName" placeholder="Leadenhall Market" /><br/>
                 <label htmlFor="location">Location:</label>
                 <input value={location} onChange={e => setLocation(e.target.value)} type="text" id="location" placeholder="London" /><br/>
                 <label   htmlFor="category">Category:</label>
                 <select value={category} onChange={e => setCategory(e.target.value)} name="category" id="category">
-                    <option value="market">Market</option>
-                    <option value="park">Park</option>
+                    <option value="Market">Market</option>
+                    <option value="Park">Park</option>
                 </select><br/>
                 <label  htmlFor="description">Description:</label>
                 <textarea value={description} onChange={e => setDescription(e.target.value)} id="description" placeholder="A beautiful covered market in the historic center of London"></textarea><br/>
-                <label htmlFor="image">Upload an image:</label>
-                <input type="file" id="image" name="image" accept='image/*' onChange={e => setImg(e.target.files)} /><br/>
+                {/* <label htmlFor="image">Upload an image:</label>
+                <input type="file" id="image" name="image" accept='image/*' onChange={e => setImg(e.target.files)} /><br/> */}
                 <button type='submit'>Add new location</button>
             </form>
         </div>
--- src/lib/action.js
+++ src/lib/action.js
@@ -7,7 +7,7 @@ import bcrypt from 'bcryptjs'
 import { postLocation, postReview } from "./data";
 
 export const handleGithubLogin = async () => {
-    await signIn("github", {callbackUrl: "/explore"})
+    await signIn("github", { callbackUrl: "/explore" })
 }
 
 export const handleLogout = async () => {
@@ -17,16 +17,16 @@ export const handleLogout = async () => {
 export const register = async (previousState, formData) => {
     const { username, email, password, img, passwordRepeat } = Object.fromEntries(formData)
     if (password !== passwordRepeat) {
-        return {error: "Passwords do not match"}
+        return { error: "Passwords do not match" }
     }
 
     try {
         connectToDatabase()
 
-        const user = await Users.findOne({username})
+        const user = await Users.findOne({ username })
 
         if (user) {
-            return {error: "Username already exists"}
+            return { error: "Username already exists" }
         }
 
         const salt = await bcrypt.genSalt(10)
@@ -42,23 +42,23 @@ export const register = async (previousState, formData) => {
         console.log(newUser);
 
         await newUser.save();
-        return {success: true}
+        return { success: true }
     } catch (err) {
         console.log(err)
-        return  { error: "Something went wrong"}
+        return { error: "Something went wrong" }
     }
 }
 
 export const login = async (previousState, formData) => {
-    const { username, password} = Object.fromEntries(formData)
+    const { username, password } = Object.fromEntries(formData)
 
     try {
-        await signIn("credentials", {username, password})
+        await signIn("credentials", { username, password })
     } catch (err) {
         console.log(err)
 
         if (err.message.includes("CredentialsSignin")) {
-            return {error: "Invalid username or password"}
+            return { error: "Invalid username or password" }
         }
         throw err;
     }
@@ -70,28 +70,57 @@ export const submitReview = async (formData) => {
         const postedReview = await postReview(formData);
         let formatedReturn = postedReview.toObject()
 
-    if (formatedReturn.location_id) formatedReturn.location_id = formatedReturn.location_id.toString();
-    if (formatedReturn.user_id) formatedReturn.user_id = formatedReturn.user_id.toString();
-    if (formatedReturn._id) formatedReturn._id = formatedReturn._id.toString();
-    const review_id = formatedReturn._id
-    if (formatedReturn.createdAt) formatedReturn.createdAt = formatedReturn.createdAt.toISOString();
+        if (formatedReturn.location_id) formatedReturn.location_id = formatedReturn.location_id.toString();
+        if (formatedReturn.user_id) formatedReturn.user_id = formatedReturn.user_id.toString();
+        if (formatedReturn._id) formatedReturn._id = formatedReturn._id.toString();
+        const review_id = formatedReturn._id
+        if (formatedReturn.createdAt) formatedReturn.createdAt = formatedReturn.createdAt.toISOString();
 
-        
-    return formatedReturn;
+
+        return formatedReturn;
     } catch (error) {
         throw new Error("failed to adding review");
     }
 };
 
-export const submitLocation = async (location) => {
-    try {
-        console.log("in action...")
-        await postLocation(location)
+export const submitLocation = async () => {
+    console.log("Whyyy")
+    const testLocation = {
+        address: "London",
+        categories: ['park'],
+        created_by: "spikeman", 
+        description: "another park",
+        latitude: 51.51123897160571,
+        longitude: -0.1708450056152344,
+        place_name: "Hyde Park"
+}
 
+    // const testLocation = {
+    //     img: "https://images.pexels.com/photos/1547813/pexels-photo-1547813.jpeg?auto=compress&cs=tinysrgb&w=600",
+    //     description: "nice park",
+    //     created_by: "spikeman",
+    //     categories: ["nature", "autumn"],
+    //     address: "London",
+    //     latitude: 51.51123897160571,
+    //     longitude: -0.1708450056152344,
+    //     place_name: "Danish Nature",
+    // }
+    try {
+        await postLocation(testLocation)
     } catch (error) {
-        console.log(error, "this is the error...")
-        throw new Error("Location could not be added")
+        console.log(error)
+        throw new Error("failed to post")
     }
+
+
+    // try {
+    //     console.log("in action...")
+    //     const result = await postLocation(location)
+    //     return result
+    // } catch (error) {
+    //     console.log(error, "this is the error...")
+    //     throw new Error("Location could not be added")
+    // }
 
 }
 
