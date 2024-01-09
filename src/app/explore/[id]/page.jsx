b19ece08fd5a58b09c8a320ea5b5127f8c767d66
hala_jash <starshiny567@gmail.com>
2024-01-09 16:08:59 +0000

    fetching current user

--- src/app/explore/[id]/page.jsx
+++ src/app/explore/[id]/page.jsx
@@ -3,17 +3,6 @@ import ReviewWrapper from '@/components/location/reviews/ReviewWrapper';
 import { getLocationById } from '@/lib/data';
 import { getUserByEmail } from '@/lib/data';
 import { auth } from '@/lib/auth';
-// WITH API FETCH
-// const getData = async (id) => {
-//     const res = await fetch(`${process.env.API_ROUTE}/api/locations/${id}`,
-//     {next: {revalidate: 3600}})
-
-//     if (!res.ok) {
-//     throw new Error("Something went wrong getting single location")
-//     }
-
-//     return res.json()
-// }
 
 const SingleLocationPage = async ({ params }) => {
 
@@ -24,19 +13,14 @@ const SingleLocationPage = async ({ params }) => {
     const user = await getUserByEmail(session.user.email)
     console.log(user, "this is the user review")
 
-    // FETCH WITH AN API
-    // let location = await getData(id)
-    // location = location[0]
-
-    // FETCH WITHOUT AN API
     let location = await getLocationById(id)
     location = location[0]
     console.log(location.place_name)
 
     return (
         <>
-        <LocationCard  location ={location}/> 
-        <ReviewWrapper id ={id} placeName ={location.place_name}/> 
+        <LocationCard  location ={location} user={user} /> 
+        <ReviewWrapper id ={id} placeName ={location.place_name} user={user}/> 
         </>
     )
 }
