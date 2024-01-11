7f6623470f19a20eaad56fbab4f2f13567d84287
Josh Holt <jholt@Stephanie-2.local>
2024-01-11 12:02:06 +0000

    touched up css for the location cards

--- src/components/explore/locationList/locationCard/LocationCard.jsx
+++ src/components/explore/locationList/locationCard/LocationCard.jsx
@@ -4,16 +4,32 @@ import Link from 'next/link'
 import SaveLocation from '@/components/saveLocation/SaveLocation'
 
 const LocationCard = ({location, user, setPageLocations}) => {
+
+    let roundedRating;
+    if (location.rating) {
+        roundedRating = location.rating.toFixed(1)
+    }
+
     return (
         <div className="card bg-base-100 shadow-xl">
             <figure><img className='w-full h-48 object-cover' src={location.img} alt="" /></figure>
 
             <div className="card-body rounded-b-lg bg-indigo-400">
+                    <div className = "absolute top-3 left-3 right-3">
                     <Link href={`/explore/${location._id}`}>
-                        <h2 className="card-title text-sm absolute top-3 left-3 right-3 text-gray-800 bg-white bg-opacity-75 p-4 rounded-lg">{location.place_name}</h2>   
+                        <h2 className="card-title text-sm  text-gray-800 bg-white bg-opacity-75 p-4 rounded-lg w-fit">{location.address}</h2>   
                     </Link>
+                            
+                        <div className="stats shadow w-40 w-fit mt-2 bg-opacity-75 rounded-lg stats-compact">
+    
+                            <div className="stat text-md">
+                                <div className="stat-title">stars out of 5</div>
+                                <div className="stat-value">{roundedRating}</div>
+                            </div>
+                            
+                        </div>
+                    </div>
                 <p>{location.description}</p>
-                <p>{location.rating} rating</p>
                 <div className={styles.footer}>
                     <div className={styles.categories}>
                     {location.categories.map(category => (
